cc6b6e9995380ee4c105300e3c41effea05bc7b7
Ali <87234097+MianajiAli@users.noreply.github.com>
2025-10-05 14:19:47 +0330

    Fix blog

--- frontend/app/blog/page.tsx
+++ frontend/app/blog/page.tsx
@@ -1,18 +1,30 @@
+import Link from "next/link";
+
+// app/blog/page.tsx
 export default async function BlogList() {
   const res = await fetch("http://127.0.0.1:8000/api/posts/", {
-    cache: "no-store",
+    cache: "no-store", // disables caching
   });
-  const posts = await res.json();
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch posts");
+  }
+
+  const postsData: { results: any[] } = await res.json();
+  const posts = postsData.results;
 
   return (
     <div className="prose mx-auto p-6">
       <h1 className="text-3xl font-bold">Blog</h1>
       <ul>
-        {posts.map((post: any) => (
+        {posts.map((post) => (
           <li key={post.id}>
-            <a href={`/blog/${post.slug}`} className="text-blue-600 underline">
+            <Link
+              href={`/blog/${post.slug}`}
+              className="text-blue-600 underline"
+            >
               {post.title}
-            </a>
+            </Link>
           </li>
         ))}
       </ul>
